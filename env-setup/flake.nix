f49175d46cc9ea75a1237d7ac88dff140a884614
cadefi <113267313+cadefi@users.noreply.github.com>
2026-04-26 01:11:46 -0400

    Update flake.nix

--- env-setup/flake.nix
+++ env-setup/flake.nix
@@ -4,11 +4,7 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs { inherit system; };
-    in
-    {
+  outputs = { self, nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefkhkh
       devShells.default = pkgs.mkShell {
         packages = with pkgs; [
           rustup
