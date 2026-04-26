66ca393fc8f4c94764bf6b6f0e6665e45fcb915f
cadefi <113267313+cadefi@users.noreply.github.com>
2026-04-26 01:12:43 -0400

    Update chain_spec.rs

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -7,12 +7,7 @@ pub type ChainSpec = sc_service::GenericChainSpec;
 pub fn development_chain_spec() -> Result<ChainSpec, String> {
 	Ok(ChainSpec::builder(
 		WASM_BINARY.ok_or_else(|| "Development wasm not available".to_string())?,
-		None,
-	)
-	.with_name("Development")
-	.with_id("dev")
-	.with_chain_type(ChainType::Development)
-	.with_genesis_config_preset_name(sp_genesis_builder::DEV_RUNTIME_PRESET)
+		None,\
 	.build())
 }
 
